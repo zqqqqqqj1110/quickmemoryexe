142e37522d4cc0f525bd606a5f407ab49c4a3cfd
zqqqqqqj1110 <913278533@qq.com>
2023-11-30 17:16:54 +0800

    闪视训练功能完善

--- app/src/SStrainComponent/OptionSS2Component.js
+++ app/src/SStrainComponent/OptionSS2Component.js
@@ -1,14 +1,40 @@
-// 随机多字闪现
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../css/OptionSS1Component.css'; // 导入样式文件
 
 const OptionSS2Component = () => {
-  return(
-  <div>
-      随机多字闪现
+  const [randomChars, setRandomChars] = useState('');
 
-  </div>
-    );
-}
+  useEffect(() => {
+    // 每隔nms执行
+    const intervalId = setInterval(() => {
+      // 读文件
+      fetch('/TXT/common.txt')
+        .then(response => response.text())
+        .then(data => {
+          // 将文本内容转化为字数组
+          const charArray = data.split('');
+
+          // 随机选择4-6个字
+          const randomCount = Math.floor(Math.random() * (6 - 4 + 1)) + 4;
+          const randomChars = charArray
+            .sort(() => Math.random() - 0.5) // 随机排序
+            .slice(0, randomCount) // 切片取随机数量的字母
+            .join(''); // 连接成字符串
+
+          setRandomChars(randomChars);
+        })
+        .catch(error => console.error('Error reading file:', error));
+    }, 1000); // 1000ms间隔
+
+    // 清理定时器
+    return () => clearInterval(intervalId);
+  }, []); // 依赖数组设空
+
+  return (
+    <div className="text">
+      {randomChars}
+    </div>
+  );
+};
 
 export default OptionSS2Component;
--- app/src/SStrainComponent/OptionSS3Component.js
+++ app/src/SStrainComponent/OptionSS3Component.js
@@ -1,14 +1,36 @@
-// 成语闪现
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../css/OptionSS1Component.css'; // 导入样式文件
 
 const OptionSS3Component = () => {
-  return(
-  <div>
-      成语闪现
+  const [randomChengyu, setRandomChengyu] = useState('');
 
-  </div>
-    );
-}
+  useEffect(() => {
+    // 每隔1000ms执行
+    const intervalId = setInterval(() => {
+      // 读文件
+      fetch('/TXT/chengyu.txt') // 文件路径根据实际情况修改
+        .then(response => response.text())
+        .then(data => {
+          // 将文本内容按中文逗号分隔成成语数组
+          const chengyuArray = data.split('，');
+
+          // 随机选择一个成语
+          const randomIndex = Math.floor(Math.random() * chengyuArray.length);
+          const randomChengyu = chengyuArray[randomIndex];
+          setRandomChengyu(randomChengyu);
+        })
+        .catch(error => console.error('Error reading file:', error));
+    }, 1000); // 1000ms间隔
+
+    // 清理定时器
+    return () => clearInterval(intervalId);
+  }, []); // 依赖数组设空
+
+  return (
+    <div className="text">
+      {randomChengyu}
+    </div>
+  );
+};
 
 export default OptionSS3Component;
