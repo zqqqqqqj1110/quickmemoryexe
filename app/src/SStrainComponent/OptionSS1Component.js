05af2228b7cbdcf8af3443cfeeb4ddbff61bc0ac
zqqqqqqj1110 <913278533@qq.com>
2023-11-30 13:39:12 +0800

    单字闪现训练

--- app/src/SStrainComponent/OptionSS1Component.js
+++ app/src/SStrainComponent/OptionSS1Component.js
@@ -1,14 +1,43 @@
-// 随机单字闪现
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const OptionSS1Component = () => {
-  return(
-  <div>
-      随机单字闪现
+  const [randomCharacter, setRandomCharacter] = useState('');
 
-  </div>
-    );
-}
+  useEffect(() => {
+    const getRandomCharacter = async () => {
+      try {
+        // 获取文件
+        const response = await fetch('../../public/TXT/common.txt');
+        const text = await response.text();
+
+        // 分割为字符数组
+        const chineseCharacters = text.trim().split('');
+
+        // 随机选择一个字
+        const randomIndex = Math.floor(Math.random() * chineseCharacters.length);
+        const randomChar = chineseCharacters[randomIndex];
+
+        setRandomCharacter(randomChar);
+      } catch (error) {
+        console.error('Error fetching or processing data:', error);
+      }
+    };
+
+    // 调用异步函数
+    getRandomCharacter();
+
+    // 定时器
+    const intervalId = setInterval(getRandomCharacter, 1000); // 1000毫秒（1秒）
+
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      单字闪现: {randomCharacter}
+    </div>
+  );
+};
 
 export default OptionSS1Component;
