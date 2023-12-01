65a018515ee7f995f479b0f6e65d942d831af7e7
zqqqqqqj1110 <913278533@qq.com>
2023-12-01 15:45:17 +0800

    照相记忆功能

--- app/src/ZXtrainComponent/OptionZX1Component.js
+++ app/src/ZXtrainComponent/OptionZX1Component.js
@@ -1,13 +1,36 @@
-// 书法图片训练
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../css/OptionZX1Component.css'
 
 const OptionZX1Component = () => {
-  return(
-    <div>
-        书法图片训练
+  const [randomImage, setRandomImage] = useState(null);
+
+  useEffect(() => {
+    // 获取public/ZXpic文件夹中的所有PNG图像文件
+    const importAll = (r) => r.keys().map(r);
+    const images = importAll(require.context('../../public/ZXpic', false, /\.(png)$/));
+
+    // 函数用于随机选择图像
+    const getRandomImage = () => {
+      const randomIndex = Math.floor(Math.random() * images.length);
+      return images[randomIndex];
+    };
+
+    // 设置初始图像路径
+    setRandomImage(getRandomImage());
+
+    const intervalId = setInterval(() => {
+      setRandomImage(getRandomImage());
+    }, 1000);   //  定时器
+
+    // 在组件卸载时清除定时器
+    return () => clearInterval(intervalId);
+  }, []); // 空依赖数组确保只在组件挂载时运行一次
+
+  return (
+    <div className="container">
+      {randomImage && <img src={randomImage} alt="Random Image" />}
     </div>
-      );
+  );
 };
 
 export default OptionZX1Component;
--- app/src/ZXtrainComponent/OptionZX2Component.js
+++ app/src/ZXtrainComponent/OptionZX2Component.js
@@ -1,13 +1,49 @@
-// 记忆训练
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../css/OptionZX2Component.css'
 
 const OptionZX2Component = () => {
-  return(
-    <div>
-        记忆训练
+  const [randomImages, setRandomImages] = useState([]);
+
+  useEffect(() => {
+    const loadRandomImages = () => {
+      // 获取public/ZXpic文件夹中的所有PNG图像文件
+      const importAll = (r) => r.keys().map(r);
+      const images = importAll(require.context('../../public/ZXpic', false, /\.(png)$/));
+
+      // 随机选择9张图像
+      const randomIndexes = [];
+      while (randomIndexes.length < 9) {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        if (!randomIndexes.includes(randomIndex)) {
+          randomIndexes.push(randomIndex);
+        }
+      }
+
+      const randomImagePaths = randomIndexes.map((index) => images[index]);
+
+      // 设置随机选择的9张图像路径
+      setRandomImages(randomImagePaths);
+    };
+
+    // 初始加载一次
+    loadRandomImages();
+
+    // 每隔1000ms切换一次图像
+    const intervalId = setInterval(() => {
+      loadRandomImages();
+    }, 1000);
+
+    // 在组件卸载时清除定时器
+    return () => clearInterval(intervalId);
+  }, []); // 空依赖数组确保只在组件挂载时运行一次
+
+  return (
+    <div className="container">
+      {randomImages.map((image, index) => (
+        <img key={index} src={image} alt={`Random Image ${index + 1}`} />
+      ))}
     </div>
-      );
+  );
 };
 
 export default OptionZX2Component;
