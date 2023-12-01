8772372f01cec5e11b6054ee39b9120f249c8cf8
zqqqqqqj1110 <913278533@qq.com>
2023-12-01 17:27:16 +0800

    子块移动

---
+++ app/src/SDtrainComponent/OptionSD1Component.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import '../css/OptionSD1Component.css'; // 导入样式文件
+
+const OptionSD1Component = () => {
+  const [startIndex, setStartIndex] = useState(0);
+  const [displayText, setDisplayText] = useState('');
+  const [position, setPosition] = useState('top-left'); // 新增状态来控制位置
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 读文件
+        const response = await fetch('/TXT/sudu.txt');
+        let data = await response.text();
+
+        // 移除所有换行符
+        data = data.replace(/\n/g, '');
+
+        // 每行包含的字符数范围
+        const charsPerLine = Math.floor(Math.random() * 5) + 8; // 8到12之间的随机数
+
+        // 计算总字符数
+        const totalChars = charsPerLine * 3;
+
+        // 计算下一个要显示的文本
+        const nextText = data.slice(startIndex, startIndex + totalChars);
+
+        // 将文本分成三行
+        const lines = [];
+        for (let i = 0; i < 3; i++) {
+          lines.push(nextText.slice(i * charsPerLine, (i + 1) * charsPerLine));
+        }
+
+        // 更新显示的文本和起始索引
+        setDisplayText(lines.join('\n'));
+        setStartIndex((startIndex + totalChars) % data.length);
+
+        // 切换位置
+        setPosition((prevPosition) => getNextPosition(prevPosition));
+      } catch (error) {
+        console.error('Error reading file:', error);
+      }
+    };
+
+    // 每隔nms执行
+    const intervalId = setInterval(fetchData, 1000); // 时间间隔
+
+    // 清理定时器
+    return () => clearInterval(intervalId);
+  }, [startIndex]); // 监听startIndex的变化
+
+  const getNextPosition = (prevPosition) => {
+    // 定义六个位置，循环切换
+    const positions = ['top-left', 'top-right', 'middle-left', 'middle-right', 'bottom-left', 'bottom-right'];
+    const currentIndex = positions.indexOf(prevPosition);
+    const nextIndex = (currentIndex + 1) % positions.length;
+    return positions[nextIndex];
+  };
+
+  return (
+    <div className={`text-container ${position}`}>
+      <p className="text-content" style={{ whiteSpace: 'pre-line' }}>
+        {displayText}
+      </p>
+    </div>
+  );
+};
+
+export default OptionSD1Component;
