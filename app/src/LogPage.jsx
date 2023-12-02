7bcca778d7e47825ac341d43b9673286cb189b74
zqqqqqqj1110 <913278533@qq.com>
2023-12-02 22:16:03 +0800

    登陆页面

--- app/src/LogPage.jsx
+++ app/src/LogPage.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
-import App from './menutop';
-import { useNavigate } from 'react-router-dom'; // 修改为 useNavigate
+import { useNavigate } from 'react-router-dom';
 import './css/LogPage.css';
 
 const LogPage = () => {
-  const navigate = useNavigate(); // 修改为 useNavigate
+  const navigate = useNavigate();
   const [form] = Form.useForm();
 
   const handleSubmit = (values) => {
     console.log('Received values of form: ', values);
-    // 在这里执行登录逻辑，根据 values 处理登录操作
 
+    // 在这里执行登录逻辑，根据 values 处理登录操作
     // 模拟登录成功，实际情况中需要根据业务逻辑来判断是否登录成功
     const loginSuccess = true;
 
@@ -26,10 +25,21 @@ const LogPage = () => {
 
   return (
     <Form form={form} onFinish={handleSubmit} className="login-form">
-      {/* ... 其他表单项 */}
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: '请输入用户名！' }]}
+      >
+        <Input placeholder="用户名" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: '请输入密码！' }]}
+      >
+        <Input type="password" placeholder="密码" />
+      </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
-          登录
+          登录/注册
         </Button>
       </Form.Item>
     </Form>
